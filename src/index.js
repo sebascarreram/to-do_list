185eb390e7607eb04e217e94d2e17ad30a309d75
Sebastian Carrera <sebascarreram@hotmail.com>
2021-07-29 19:20:25 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./sass/main.scss";
+
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App tasks={DATA} />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
